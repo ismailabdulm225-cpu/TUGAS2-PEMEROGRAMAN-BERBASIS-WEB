b2ca02730eaf6fda9fcaa4c66d3df3d5adc4183e
Ismail <ismailabdulm225@gmail.com>
2026-05-17 23:39:12 +0700

    Update index-app.js

--- js/index-app.js
+++ js/index-app.js
@@ -1,37 +1,27 @@
 const app = Vue.createApp({
-
     data() {
         return {
             greeting: ''
         }
     },
-
+    
     mounted() {
         this.setGreeting()
     },
 
     methods: {
-
         setGreeting() {
-
             const hour = new Date().getHours()
-
             if (hour < 12) {
                 this.greeting = 'Selamat Pagi'
             }
-
             else if (hour < 18) {
                 this.greeting = 'Selamat Sore'
             }
-
             else {
                 this.greeting = 'Selamat Malam'
             }
-
         }
-
     }
-
 })
-
 app.mount('#app')
