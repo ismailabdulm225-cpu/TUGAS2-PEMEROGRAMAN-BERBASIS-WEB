9a163667bf3721144c94ed031df06ed1067a4b64
ismailabdulm225-cpu <ismailabdulm225@gmail.com>
2026-05-18 22:22:28 +0700

    fix vue error

--- js/index-app.js
+++ js/index-app.js
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 const app = Vue.createApp({
     data() {
         return {
             greeting: ''
         }
     },
-    
+
     mounted() {
         this.setGreeting()
     },
@@ -13,6 +12,7 @@ const app = Vue.createApp({
     methods: {
         setGreeting() {
             const hour = new Date().getHours()
+
             if (hour < 12) {
                 this.greeting = 'Selamat Pagi'
             }
@@ -25,33 +25,5 @@ const app = Vue.createApp({
         }
     }
 })
-app.mount('#app')
-=======
-const app = Vue.createApp({
-    data() {
-        return {
-            greeting: ''
-        }
-    },
-    
-    mounted() {
-        this.setGreeting()
-    },
 
-    methods: {
-        setGreeting() {
-            const hour = new Date().getHours()
-            if (hour < 12) {
-                this.greeting = 'Selamat Pagi'
-            }
-            else if (hour < 18) {
-                this.greeting = 'Selamat Sore'
-            }
-            else {
-                this.greeting = 'Selamat Malam'
-            }
-        }
-    }
-})
 app.mount('#app')
->>>>>>> 0502e7c99e052c539a9c48de8b044e208a6b5f39
